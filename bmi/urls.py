4d7af65c39d742eaeb394d37a13efd2e25e56c4f
teamkosy <shimwh81@naver.com>
2021-02-23 17:31:41 +0900

    0223

--- bmi/urls.py
+++ bmi/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.bmi),
+    path('bmiOk', views.bmiOk),
 ]
--- bmi/views.py
+++ bmi/views.py
@@ -3,20 +3,25 @@ from django.shortcuts import render
 # Create your views here.
 # from django.http import HttpResponse
 
-
 def bmi(request):
-    w1 = request.POST['w']
-    h1 = request.POST['h']
+    return render(request, 'infoweb/index.html')
 
+def bmiOk(request):
+    if request.method == 'POST':
 
-    w = int(75)
-    h = int(168)
-    bmi = w / (h / 100) ** 2
-    if bmi < 18.5:
+        w1 = request.POST.get('w')
+        h1 = request.POST.get('h')
 
-        return render(request,'infoweb/index.html', {'bmi' : "저체중 : 마른편입니다. 조금 더 먹어도 괜찮아요^^"})
-    if bmi < 25:
+        if len(w1) == 0 or len(h1) == 0:
+            # flash('측정 값을 입력하세요.')
+            return render_template('infoweb/index.html')
+        else:
+            w = int(w1)
+            h = int(h1)
+            bmi = w / (h / 100) ** 2
 
-        return render(request,'infoweb/index.html', {'bmi' : "정상 :적당하네요^^ 잘 유지하세요~"})
-
-    return render(request,'infoweb/index.html', {'bmi' : "비만 ㅠㅠ : 다이어트가 필요합니다~"})
+            if bmi < 18.5:
+                return render(request,'infoweb/index.html', context={'bmi' : "저체중 : 마른편입니다. 조금 더 먹어도 괜찮아요^^"})
+            if bmi < 25:
+                return render(request,'infoweb/index.html', context={'bmi' : "정상 :적당하네요^^ 잘 유지하세요~"})
+            return render(request,'infoweb/index.html', context={'bmi' : "비만 ㅠㅠ : 다이어트가 필요합니다~"})
