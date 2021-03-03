fb89b01e58cae28ab65df1bca6edf04a5b7f2ff5
teamkosy <shimwh81@naver.com>
2021-03-04 03:06:56 +0900

    0304

--- member/urls.py
+++ member/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.member, name='member'),
+    path('join/', views.join, name='join'),
+    path('joinOk/', views.joinOk, name='joinOk'),
 ]
--- member/views.py
+++ member/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def member(request):
     return render(request,'infoweb/member.html')
+
+def join(request):
+    return render(request,'infoweb/join.html')
+
+def joinOk(request):
+    return render(request,'infoweb/index.html')
