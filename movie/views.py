825cd11f780c2b7fe67d29cc3401d5d68d5bea3f
teamkosy <shimwh81@naver.com>
2021-03-07 15:20:07 +0900

    0307

--- movie/views.py
+++ movie/views.py
@@ -1,37 +1,16 @@
 from django.shortcuts import render
 import requests
-# import json
 
 def movie(request):
-    url = 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f9e46ed8e2b842376cd516c304c7fde2&targetDt=20201107'
+    url = 'https://www.kobis.or.kr/kobis/business/main/searchMainDailyBoxOffice.do'
     response = requests.get(url)
-    response.status_code
+    movie = response.json()
+    date = movie[0]['startDate']
 
-    data = response.json()
-
-    r_list = []
+    movie_list = []
     for i in range(0, 10):
-        # print(i)
-        j = data['boxOfficeResult']['dailyBoxOfficeList'][i]['movieNm']
-        r_list.append(j)
+        rs = movie[i]
+        a = (rs['rank'], rs['movieNm'], rs['openDt'], rs['salesAmt'], rs['audiCnt'], rs['rankInten'])
+        movie_list.append(a)
 
-    open_list = []
-    for i in range(0, 10):
-        # print(i)
-        j = data['boxOfficeResult']['dailyBoxOfficeList'][i]['openDt']
-        open_list.append(j)
-
-    audiAcc_list = []
-    for i in range(0, 10):
-        # print(i)
-        j = data['boxOfficeResult']['dailyBoxOfficeList'][i]['audiAcc']
-        audiAcc_list.append(j)
-
-    audiAcc_list = [int(i) for i in audiAcc_list]
-
-    total = []
-    for i in range(0, 10):
-        j = [r_list[i], open_list[i], audiAcc_list[i]]
-        total.append(j)
-
-    return render(request, 'infoweb/movie.html', {'total':total})
+    return render(request, 'infoweb/movie.html', {'movie_list':movie_list, 'date':date})
