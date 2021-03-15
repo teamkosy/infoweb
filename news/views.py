0643187b74d74c0be6e39c53f2124f4d1d008bdb
teamkosy <shimwh81@naver.com>
2021-03-15 21:50:19 +0900

    0315

--- news/views.py
+++ news/views.py
@@ -6,8 +6,31 @@ def news(request):
     url = 'https://news.daum.net/'
     response = requests.get(url, verify=False)
     text = response.text
-    soup = BeautifulSoup(text)
-    news = soup.select_one('ol.list_popcmt')
-    newslist = news.text.replace('\n', '').replace(' ', '')
-    # print(newslist)
-    return render(request, 'infoweb/news.html', context={'newslist': newslist})
+    soup = BeautifulSoup(text, 'html.parser')
+
+    title = soup.select_one('h3.tit_news').text # 기사 분류 타이틀
+
+    # top5 기사
+    data_list = []
+    news1 = soup.select_one('#mArticle > div.box_peruse > div > ol > li:nth-child(1)')
+    data = news1.text.replace('\n', '').replace(' ', '').replace('\'', '')
+    data_list.append(data)
+    news2 = soup.select_one('#mArticle > div.box_peruse > div > ol > li:nth-child(2)')
+    data = news2.text.replace('\n', '').replace(' ', '').replace('\'', '')
+    data_list.append(data)
+    news3 = soup.select_one('#mArticle > div.box_peruse > div > ol > li:nth-child(3)')
+    data = news3.text.replace('\n', '').replace(' ', '').replace('\'', '')
+    data_list.append(data)
+    news4 = soup.select_one('#mArticle > div.box_peruse > div > ol > li:nth-child(4)')
+    data = news4.text.replace('\n', '').replace(' ', '').replace('\'', '')
+    data_list.append(data)
+    news5 = soup.select_one('#mArticle > div.box_peruse > div > ol > li:nth-child(5)')
+    data = news5.text.replace('\n', '').replace(' ', '').replace('\'', '')
+    data_list.append(data)
+
+    #기사 링크
+    link_list = []
+    for i in soup.find('ol', class_='list_popcmt').find_all('li'):
+        link_list.append(i.find("a")["href"])
+
+    return render(request, 'infoweb/news.html', context={'title':title, 'data_list': data_list, 'link_list':link_list})
