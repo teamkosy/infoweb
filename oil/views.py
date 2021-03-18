10f1959b83853f23ae25624017da0394f3371031
teamkosy <shimwh81@naver.com>
2021-03-18 12:29:14 +0900

    0318

--- oil/views.py
+++ oil/views.py
@@ -10,6 +10,14 @@ def oil(request):
     soup = BeautifulSoup(text, 'html.parser')
     row = soup.select_one('ul.lsttype_tb')
     oil = row.text.replace(' ', '')
-    oil
 
-    return render(request,'infoweb/oil.html', {'oil':oil})
+    #휘발유
+    oil1 = soup.select_one(
+        '#main_pack > section.sc_new.cs_gold > div > div.api_cs_wrap.contents03 > div > div.ar_cont > div.cont_dtcon.cont_tb > div > ul:nth-child(2) > li:nth-child(1) > dl > dd > strong').text
+    #경유
+    oil2 = soup.select_one(
+        '#main_pack > section.sc_new.cs_gold > div > div.api_cs_wrap.contents03 > div > div.ar_cont > div.cont_dtcon.cont_tb > div > ul:nth-child(2) > li:nth-child(3) > dl > dd > strong').text
+
+    day = soup.select_one('#main_pack > section.sc_new.cs_gold > div > div.api_cs_wrap.contents03 > div > div.ar_cont > div.cont_dtcon.cont_tb > div > ul:nth-child(2) > li:nth-child(1) > dl > dt > em').text
+
+    return render(request,'infoweb/oil.html', {'oil':oil, 'oil1':oil1, 'oil2':oil2, 'day':day})
