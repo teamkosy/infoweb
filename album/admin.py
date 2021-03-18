328b2b343b0707196a90595a0126ffb80e5b4397
teamkosy <shimwh81@naver.com>
2021-03-18 20:59:43 +0900

    0318

--- album/admin.py
+++ album/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Myalbum
+from .models import Album
 
-admin.site.register(Myalbum)
+admin.site.register(Album)
--- album/urls.py
+++ album/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.album, name="album"),
-    # path('album_write', views.album_write, name="album_write"),
-    # path('album_insert', views.album_insert, name="album_insert"),
-    # path('album_view', views.album_view, name="album_view"),
-    # path('album_edit', views.album_edit, name="album_edit"),
-    # path('album_update', views.album_update, name="album_update"),
-    # path('album_delete', views.album_delete, name="album_delete"),
+    path('album_write', views.album_write, name="album_write"),
+    path('album_insert', views.album_insert, name="album_insert"),
+    path('album_view', views.album_view, name="album_view"),
+    path('album_edit', views.album_edit, name="album_edit"),
+    path('album_update', views.album_update, name="album_update"),
+    path('album_delete', views.album_delete, name="album_delete"),
 ]
